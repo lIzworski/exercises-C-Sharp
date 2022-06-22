ad7a2c9ef13249cf47f7d98213769cf0cadd7339
Lucjan Izworski <107995480+lIzworski@users.noreply.github.com>
2022-06-22 16:10:48 +0200

    Add files via upload

---
+++ z20/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Zadanie_V_0._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p1  = new Program();
+            LWymierna lw1 = new LWymierna(2,4);
+            LWymierna lw2 = new LWymierna(1,3);
+            LWymierna lw3 = new LWymierna(1,2);
+
+            Console.WriteLine("Dodawanie");
+            lw1.dodajUlamek(lw3);
+            lw1.wypiszUlamek();
+            Console.WriteLine("Odejmowanie");
+            lw2.odejmijUlamek(lw3);
+            lw2.wypiszUlamek();
+        }
+        public struct LWymierna
+        {
+            private int licznik;
+            private int mianownik;
+
+            public int Licznik { get { return licznik; } set { licznik = value; } }
+            public int Mianownik { get { return mianownik; }set{mianownik = value; }}
+
+            public LWymierna(int licznik, int mianownik) : this()
+            {
+                Licznik = licznik;
+                if(mianownik == 0)
+                {
+                Mianownik = 1;
+                }
+                else
+                {
+                    Mianownik = mianownik;
+                }
+            }
+
+            public LWymierna(int licznik) : this()
+            {
+                Licznik = licznik;
+                Mianownik = 1;
+            }
+
+            public void wczytajUlamek()
+            {
+                Console.WriteLine("Podaj wartość Licznika");
+                Licznik = int.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj wartość Mianownika");
+                Mianownik = int.Parse(Console.ReadLine());
+               if(Mianownik == 0) { Mianownik = 1; }
+            }
+            public void wypiszUlamek()
+            {
+                Console.Write($"{Licznik}/{Mianownik}");
+                if(Licznik >= Mianownik && Licznik % Mianownik == 0 ) 
+                {
+                    int ileRazyWiekszyLiczebnik = Licznik / Mianownik;
+                    int wynik =  1 * ileRazyWiekszyLiczebnik;
+                    Console.WriteLine($" = {wynik}");
+
+
+                }
+                Console.WriteLine();
+            }
+            public void dodajUlamek(LWymierna B)
+            {
+                int TmpLicznik;
+                int TmpMianownik;
+                int LicznikA =  this.Licznik;
+                int MianownikA = this.Mianownik;
+                int LicznikB = B.Licznik;
+                int MianownikB = B.Mianownik;
+
+                if(MianownikA == MianownikB)
+                {
+                    TmpLicznik = this.Licznik += B.Licznik;
+                    TmpMianownik = this.Mianownik;
+
+                }
+                else
+                {
+
+                    LicznikA = this.Licznik * B.Mianownik;
+                    LicznikB = B.Licznik * this.Mianownik;
+                    TmpLicznik = LicznikA + LicznikB;
+                    TmpMianownik = this.Mianownik * B.Mianownik;
+
+                    this.Licznik = TmpLicznik;
+                    this.Mianownik = TmpMianownik;
+                }
+            }
+            public void odejmijUlamek(LWymierna B)
+            {
+                int TmpLicznik;
+                int TmpMianownik;
+                int LicznikA = this.Licznik;
+                int MianownikA = this.Mianownik;
+                int LicznikB = B.Licznik;
+                int MianownikB = B.Mianownik;
+
+                if (MianownikA == MianownikB)
+                {
+                    TmpLicznik = this.Licznik -= B.Licznik;
+                    TmpMianownik = this.Mianownik;
+
+                }
+                else
+                {
+
+                    LicznikA = this.Licznik * B.Mianownik;
+                    LicznikB = B.Licznik * this.Mianownik;
+                    TmpLicznik = LicznikA - LicznikB;
+                    TmpMianownik = this.Mianownik * B.Mianownik;
+
+                    this.Licznik = TmpLicznik;
+                    this.Mianownik = TmpMianownik;
+                }
+            }
+        }
+    }
+}
---
+++ z21/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        double[] wspolrzedne = new double[] { 1,2,5 };
+        Wektor w1 = new Wektor(3, wspolrzedne);
+        w1.Show();
+        Console.WriteLine();
+        Console.WriteLine("Suma współrzędnych wektora wynosi: "+w1.Suma());
+        Wektor w2 = new Wektor(3, wspolrzedne);
+        w2.Show();
+        Console.WriteLine();
+        Wektor w3 = w1.add(w2);
+        w3.Show();
+    }
+}
+class Wektor
+{
+    private int Wymiar;
+    private double[] Wspolrzedne;
+    public Wektor(int n, double[] dane)
+    {
+        Wymiar = n;
+        Wspolrzedne = new double[n];
+        for (int i = 0; i < Wymiar; i++)
+        {
+            Wspolrzedne[i] = dane[i];
+        }
+    }
+    public void Show()
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < Wymiar; i++)
+        {
+            Console.Write(Wspolrzedne[i]+" ");
+        }
+        Console.Write("]");
+    }
+    public double Suma()
+    {
+        double wynik = 0;
+        for (int i = 0; i < Wymiar; i++)
+        {
+            wynik +=Wspolrzedne[i];
+        }
+        return wynik;
+    }
+    public Wektor add(Wektor w2)
+    {
+        double[] wynik = new double[Wymiar];
+        for (int i = 0; i < Wymiar; i++)
+        {
+            wynik[i] = Wspolrzedne[i] + w2.Wspolrzedne[i];
+        }
+        return new Wektor(Wymiar, wynik);
+    }
+}
