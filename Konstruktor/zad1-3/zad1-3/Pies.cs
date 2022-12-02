fbf3b0abb37d83155660fb20c6484d3ba2f0a011
Lucjan Izworski <107995480+lIzworski@users.noreply.github.com>
2022-12-02 14:53:49 +0100

    Add files via upload

---
+++ Konstruktor/zad1-3/zad1-3/Pies.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1_3
+{
+    internal class Pies
+    {
+        private string _name = "";
+        private int _age = 0;
+        private string _rasa = "";
+        private static int _howManyDogs = 0;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Age { get { return _age; } set { _age = value; } }
+        public string Rasa { get { return _rasa; } set { _rasa = value; } }
+        
+
+        public Pies(string name, int age, string rasa = "Mongrel")
+        {
+            Name = name;
+            Age = age;
+            Rasa = rasa;
+            _howManyDogs++;
+        }
+        public static void HowManyDogs()
+        {
+            Console.WriteLine($"Ilość psów : {_howManyDogs}");
+        }
+    }
+}
---
+++ Konstruktor/zad1-3/zad1-3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace zad1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pies p1 = new Pies("Rocky", 7);
+            Pies p2 = new Pies("Sone", 7,"Chow Chow");
+            Pies p3 = new Pies("HotDog", 7,"rodwailer");
+            Pies p4 = new Pies("Pies", 7, "Ciualua");
+            Pies p5 = new Pies("Pies2", 7);
+            Pies p6 = new Pies("RudyPies", 7);
+            Pies p7 = new Pies("białyPies", 7,"Rablador");
+
+            Pies.HowManyDogs();
+
+
+        }
+    }
+}
