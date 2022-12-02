5b338e428f1dce7bfdcb729203b3f20c65fde4d5
Lucjan Izworski <107995480+lIzworski@users.noreply.github.com>
2022-12-02 14:56:23 +0100

    Add files via upload

---
+++ własciwosci/zad1/zad1/Osoba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Osoba
+    {
+        private string _name = "";
+        private string _surname = "";
+        private int _age = 0;
+        private string _sex = "";
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public int Age { get { return _age; } set { if (_age > 0 && _age < 101) { _age = value; } } }
+        public string Sex { get { return _sex; } set { if (_sex == "kobieta" || _sex == "mężczyzna" || _sex == "inna") { _sex = value; } } }
+
+    }
+}
